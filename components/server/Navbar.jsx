9c8cd2309c36824faedcf79b445619df492441ab
Karam Abbas <kma69362@gmail.com>
2026-05-13 13:57:28 -0700

    Revert "Removed about me page"
    This reverts commit b641b4a5a60b53b5f0be3637fed2d030e88c10c9.

--- components/server/Navbar.jsx
+++ components/server/Navbar.jsx
@@ -6,6 +6,7 @@ const Navbar = () => {
             <ul className="flex space-x-4">
                 <h2 className="text-xl font-bold">Karam Abbas</h2>
                 <li className="hover:bg-gray-600"><a className="text-white hover:text-gray-300 no-underline" href="/">Home</a></li>
+                <li className="hover:bg-gray-600"><a className="text-white hover:text-gray-300 no-underline" href="/about">About</a></li>
                 <li className="hover:bg-gray-600"><a className="text-white hover:text-gray-300 no-underline" href="/projects">Projects</a></li>
                 <li className="hover:bg-gray-600"><a className="text-white hover:text-gray-300 no-underline" href="/contact">Contact</a></li>
                 <li className="ml-auto"><ThemeToggle/></li>
